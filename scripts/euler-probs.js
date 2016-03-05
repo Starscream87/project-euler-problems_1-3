0bee29bd8df9d0ad37b7428a6a134b65e6675306
Starscream87 <dr_wall22@yahoo.com>
2016-03-05 12:08:30 -0500

    Update and rename euler-prob1.js to euler-probs.js

--- scripts/euler-probs.js
+++ scripts/euler-probs.js
@@ -8,8 +8,7 @@ var prob1Input = document.getElementById("multiplesInput").value,
 		}	
 	}
 document.getElementById("answer_1").innerHTML = sum;
-}
-
+};
 document.getElementById("multiples").addEventListener("click", answer_1);
 
 //Problem 2 Even Fibonacci numbers
@@ -28,7 +27,7 @@ var prob2Input = document.getElementById("fibonacciInput").value,
 		num2 = num3;
 	}
 	document.getElementById("answer_2").innerHTML = evenSum;
-	};
+};
 document.getElementById("fibonacci").addEventListener("click", answer_2);
 
 //Problem 3 Largest Prime Factor
@@ -50,55 +49,11 @@ var n = document.getElementById("primeFactorInput").value,
   }
   if (primeFactor == 0){
 	  alert(n + " is a prime factor of itself.")
-	} else {
-			document.getElementById("answer_3").innerHTML = primeFactor.pop();
-		}
+  } else {
+	document.getElementById("answer_3").innerHTML = primeFactor.pop();
+	}
 };
 document.getElementById("largestPF").addEventListener("click", answer_3);
 
 
-
-// Select a problem
-/*var problems = document.getElementById("problems");
-var items = problems.querySelectorAll("span");
-
-
-for (var i = 0; i < items.length; i++) {
-	items[i].addEventListener('click', selectProblem);
-}
-
-function selectProblem(){
-	var current = this.id;
-	var index = current.split("_")[1];
-	for (var i = 0; i < items.length; i++){
-		if (items[i].classList.contains("selected")){
-			items[i].classList.remove("selected");
-		} 
-		document.getElementById("problem_" + (i + 1).style.display = "none";
-	}
-	document.getElementById(current).className = "selected";
-	document.getElementById("problem_" + index).style.display = "block";
-}*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Select a problem will be completed at a later time
