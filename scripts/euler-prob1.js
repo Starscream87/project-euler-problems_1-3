dc3b644b994f2962cccbd593593b3a039cb22470
Starscream87 <dr_wall22@yahoo.com>
2016-03-05 11:03:09 -0500

    Add input values to problems 1,2, and 3

--- scripts/euler-prob1.js
+++ scripts/euler-prob1.js
@@ -1,8 +1,8 @@
 // Problem 1 Multiples of 3 and 5
-/*var answer_1 = function ans1() {
-var sum = 0;
-var i = 0;
-	for(i ; i < 1000; i++) {
+var answer_1 = function ans1() {
+var prob1Input = document.getElementById("multiplesInput").value,
+	i = 0, sum = 0;
+	for(i ; i < prob1Input; i++) {
 		if((i % 3 === 0) || (i % 5 === 0)){
 			sum += i;
 		}	
@@ -10,34 +10,32 @@ var i = 0;
 document.getElementById("answer_1").innerHTML = sum;
 }
 
-document.getElementById("bttn").addEventListener("click", answer_1);
+document.getElementById("multiples").addEventListener("click", answer_1);
 
 //Problem 2 Even Fibonacci numbers
 var answer_2 = function ans2() {
-var num1 = 1;
-var num2 = 2;
-var num3 = num1 + num2;
-var evenSum = 0;
-	while (num1 < 4000000) {
-		num1 = num2 ;
-		num2 = num3 ;
-		num3 = num1  + num2;
-		console.log(num1);
-		if (num1 % 2 === 0){
-			evenSum += num1;
+var prob2Input = document.getElementById("fibonacciInput").value,
+	num1 = 1, num2 = 1, num3 = 0, evenSum = 0;
+	if (prob2Input < 2) {
+		alert("Please enter a number greater than 2")
+	}
+	while (num3 < prob2Input) {
+		if (num3 % 2 === 0){
+			evenSum += num3;
 		}
+		num3 = num1 + num2;
+		num1 = num2;
+		num2 = num3;
 	}
 	document.getElementById("answer_2").innerHTML = evenSum;
 	};
-document.getElementById("bttn").addEventListener("click", answer_2);*/
+document.getElementById("fibonacci").addEventListener("click", answer_2);
 
 //Problem 3 Largest Prime Factor
 var answer_3 = function largestPF() {
-  var n = 13195, x = 3,
-    j, i = 2,
-    primeFactor = [],
-	isPrime;
-  for (x; x <= n; x += 2) {
+var n = document.getElementById("primeFactorInput").value, 
+	x = 3, j, i = 2, primeFactor = [], isPrime;
+  for (x; x < n; x += 2) {
     isPrime = true;
     j = Math.sqrt(x);
     for (i = 2; i <= j; i++) {
@@ -50,9 +48,13 @@ var answer_3 = function largestPF() {
       primeFactor.push(x);
     }
   }
-  document.getElementById("answer_3").innerHTML = primeFactor.pop();
+  if (primeFactor == 0){
+	  alert(n + " is a prime factor of itself.")
+	} else {
+			document.getElementById("answer_3").innerHTML = primeFactor.pop();
+		}
 };
-document.getElementById("bttn").addEventListener("click", answer_3);
+document.getElementById("largestPF").addEventListener("click", answer_3);
 
 
 
